b0663d166a96fc46895163cfa32baa4d91a11652
Malavika Mysore <malavika.mysore@gmail.com>
2022-05-17 14:27:53 +0100

    #3 GET /api/topics

--- app.js
+++ app.js
@@ -4,4 +4,12 @@ const app = express();
 
 app.get("/api/topics", getTopics);
 
+// ERROR HANDLING:
+app.use("*", (req, res, next) => {
+  res.status(404).send({ msg: "Path not found" });
+});
+app.use((err, req, res, next) => {
+  res.status(500).send({ msg: "Internal Server Error" });
+});
+
 module.exports = app;
