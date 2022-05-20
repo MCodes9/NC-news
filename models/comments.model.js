e98fe103e70a0bef123db94ad352b966712f17f1
Malavika Mysore <malavika.mysore@gmail.com>
2022-05-20 13:42:00 +0100

    Ticket 9: Responds with an array of comments

--- models/comments.model.js
+++ models/comments.model.js
@@ -1,7 +1,7 @@
 const db = require("../db/connection");
 
 exports.fetchComments = (articleId) => {
-  let queryStr = "SELECT * FROM comments WHERE article_id = $1;";
+  const queryStr = "SELECT * FROM comments WHERE article_id = $1;";
   return db.query(queryStr, [articleId]).then(({ rows }) => {
     return rows;
   });
